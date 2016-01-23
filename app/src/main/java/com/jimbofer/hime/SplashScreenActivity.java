2a72dca41e899096b31cc6f4de01e905cf331849
Shanyl Jimenez <falsenoglory@gmail.com>
2016-01-24 03:55:53 +0800

    Implimented Splash screen and Home Screen

--- app/src/main/java/com/jimbofer/hime/SplashScreenActivity.java
+++ app/src/main/java/com/jimbofer/hime/SplashScreenActivity.java
@@ -7,13 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 5000;
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
         new Handler().postDelayed(new Runnable() {
 
             @Override
