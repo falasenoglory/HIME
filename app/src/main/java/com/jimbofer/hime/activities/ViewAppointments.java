e7e9e2e60bf8a5a2cb9d34212749fee2abe46200
chanferolino <chanferolino@gmail.com>
2016-03-14 09:40:20 +0800

    Added getAlltransactionOfPatient in parseTransaction Added UI of the actor "Doctor"

---
+++ app/src/main/java/com/jimbofer/hime/activities/ViewAppointments.java
@@ -0,0 +1,73 @@
+package com.jimbofer.hime.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.jimbofer.hime.R;
+import com.jimbofer.hime.fragments.ListViewFragmentAppointment;
+
+/**
+ * Created by Christian on 3/14/2016.
+ */
+public class ViewAppointments extends AppCompatActivity {
+    private ListViewFragmentAppointment mListViewFragment;
+    private Menu menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_patients);
+
+        Intent intent= getIntent();
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        mListViewFragment = ListViewFragmentAppointment.newInstance();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, mListViewFragment)
+                .commit();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        this.menu = menu;
+
+//        if (position == -1) {
+//            menu.getItem(0).setIcon(getResources().getDrawable(R.drawable.ic_done));
+//            menu.getItem(1).setVisible(false);
+//            // mi.setIcon(R.drawable.ic_done);
+//        }
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        if(item.getItemId()== R.id.action_edit)
+        {
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
--- app/src/main/java/com/jimbofer/hime/adapters/ViewAppointmentRequest.java
+++ app/src/main/java/com/jimbofer/hime/adapters/ViewAppointmentRequest.java
@@ -1,7 +1,102 @@
 package com.jimbofer.hime.adapters;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jimbofer.hime.ParseUtils.ParseAppointments;
+import com.jimbofer.hime.R;
+import com.jimbofer.hime.model.Appointments;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
 /**
  * Created by Christian on 3/13/2016.
  */
-public class ViewAppointmentRequest {
+public class ViewAppointmentRequest extends ArrayAdapter<Appointments> {
+
+    private Context mContext;
+    private int         mLayoutId;
+    private List<Appointments> mAppointments;
+
+    public ViewAppointmentRequest(Context context, int resource, List<Appointments> appointment) {
+        super(context, resource, appointment);
+
+        mContext = context;
+        mLayoutId = resource;
+        mAppointments = appointment;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final ViewHolder viewHolder;
+
+        if (convertView == null) {
+            // Inflate the layout
+            convertView = LayoutInflater.from(mContext).inflate(mLayoutId, parent, false);
+
+            // create the view holder
+            viewHolder = new ViewHolder();
+            viewHolder.txtDate = (TextView) convertView.findViewById(R.id.dayA);
+            viewHolder.txtMonth = (TextView) convertView.findViewById(R.id.monthA);
+            viewHolder.txtName = (TextView) convertView.findViewById(R.id.PatientName2);
+            viewHolder.txtStatus= (TextView) convertView.findViewById(R.id.Status);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        Appointments appt = mAppointments.get(position);
+
+        if(appt != null) {
+            String date = appt.getAppointmentDate();
+            String day = date.split("/")[1];
+            String month = date.split("/")[0];
+            viewHolder.txtDate.setText(day);
+            viewHolder.txtMonth.setText(month);
+            viewHolder.txtStatus.setText(appt.getStatus());
+            String patientID = appt.getAppointmentPatientID();
+            ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Patient");
+            query.whereEqualTo("PatientID", patientID);
+            query.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> list, ParseException e) {
+                    if (e == null) {
+
+                        for (ParseObject obj : list) {
+                            viewHolder.txtName.setText(obj.getString("firstName") + "," + obj.getString("lastName"));
+                        }
+
+
+                    } else {
+                    }
+                }
+            });
+
+        }
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        public TextView  txtName;
+        public TextView  txtStatus;
+        public TextView  txtDate;
+        public TextView txtMonth;
+    }
+
+
+
 }
